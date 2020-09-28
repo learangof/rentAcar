495a4543772fb35a24c57e0098d37dc3be060e63
Luis Arango <learangof@outlook.com>
2020-09-28 19:22:47 +1300

    fix DB

--- back/php/db_connect.php
+++ back/php/db_connect.php
@@ -1,12 +1,14 @@
 <?php
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
    
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "rentacar";
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
     if($conn->connect_error){
         die("Connection Failed: " . $conn->connect_error);
     }
